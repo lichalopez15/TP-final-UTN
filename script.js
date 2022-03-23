8211870c9280e7e31e3bfd592930ad8a0c9ec156
lichalopez15 <luisemilio93@gmail.com>
2022-03-23 19:51:22 -0300

    nuevos cambios actualizados

---
+++ script.js
@@ -0,0 +1,14 @@
+
+let botonPlay = document.getElementById ("botonPlay");
+
+let botonPausa = document.getElementById ("botonPausa");
+
+let video = document.querySelector ('video');
+
+botonPlay.addEventListener ('click',()=>{
+    video.play ()
+});
+
+botonPausa.addEventListener ('click',()=>{
+    video.pause()
+});
