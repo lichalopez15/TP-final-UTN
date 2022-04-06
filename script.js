9671c4378ed8a68e627ef7a615851c685d1bf392
lichalopez15 <luisemilio93@gmail.com>
2022-04-06 16:32:50 -0300

    pagina3 6abril

--- script.js
+++ script.js
@@ -13,4 +13,36 @@ botonPausa.addEventListener ('click',()=>{
     video.pause()
 });
 
+let puzzle = document.getElementById ('puzzle1');
+
+let caja1 = document.getElementById ('box1');
+
+puzzle.addEventListener ('dragstart',inicioTraslado);
+
+puzzle.addEventListener ('dragend',finalDeTraslado);
+
+function inicioTraslado (evento){
+    evento.dataTransfer.setData ('Text',"../Rompe1.png");
+};
+
+function finalDeTraslado (evento) {
+    puzzle.style.visibility="hidden";
+}
+
+
+
+caja1.addEventListener ('dragover',prevenirDefault);
+
+caja1.addEventListener ('drop',soltarElemento);
+
+function soltarElemento (evento) {
+
+    let dataImagen= evento.dataTransfer.getData ('Text');
+    caja1.innerHTML= `<img id="puzzle1" src= "${dataImagen}"/>`
+};
+
+function prevenirDefault (evento) {
+    evento.preventDefault()
+}
+
 
